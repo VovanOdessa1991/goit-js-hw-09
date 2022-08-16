239d9f49bb06fb90773bee24712c685994bdc7c2
VovanOdessa1991 <50461900+VovanOdessa1991@users.noreply.github.com>
2022-08-16 16:30:07 +0000

    Deploying to gh-pages from @ VovanOdessa1991/goit-js-hw-09@a7cd74f94000de5ef48d45cf0e393ebd9939a7a6 🚀

---
+++ 01-color-switcher.60268dd4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,timerId=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.60268dd4.js.map
