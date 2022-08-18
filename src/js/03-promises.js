8b9d151ee08ec8cefc08cbb63cd63e26d513933e
VovanOdessa1991 <tipodessa14@gmail.com>
2022-08-18 17:57:14 +0300

    debug for promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,36 +13,39 @@ form.addEventListener("submit", submit);
 // form.preventDefault();
 
 function submit(event) {
-  event.preventDefault();  
-  console.log()
-//  let {
-//     elements: { delay= delay.value, step=step.value, amount= amount.value  }
-//  } = event.currentTarget;
+  event.preventDefault();
+  // console.log()
+  //  let {
+  //     elements: { delay= delay.value, step=step.value, amount= amount.value  }
+  //  } = event.currentTarget;
   let delay = event.currentTarget.delay.value;
   const step = event.currentTarget.step.value;
   const amount = event.currentTarget.amount.value;
 
+  event.currentTarget.amount.value = null;
+  event.currentTarget.step.value = null;
+  event.currentTarget.delay.value = null;
+
   
   
- console.log("amount =>",amount );
-  for (let position = 1; position <= amount; position++){
-    console.log("Шаг =>",position );
+  // console.log("amount =>", amount);
+  for (let position = 1; position <= amount; position++) {
+    // console.log("Шаг =>", position);
     
     createPromise(position, delay)
-      .then(({ position, delay }) =>
-      {
+      .then(({ position, delay }) => {
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
 
-    .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-    delay = parseInt(delay)+ parseInt(step);
-    console.log("delay", delay);
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay = parseInt(delay) + parseInt(step);
+    // console.log("delay", delay);
   }
-  console.log("test3");
+  // console.log("test3");
   
-
+}
 
 
 function createPromise(position, delay) {
