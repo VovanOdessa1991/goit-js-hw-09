988cdf1c81e2f98f71ba7f6513044fc85b7eee2d
VovanOdessa1991 <tipodessa14@gmail.com>
2022-08-19 10:28:24 +0300

    debug

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,11 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
 
 const form = document.querySelector('form');
 
